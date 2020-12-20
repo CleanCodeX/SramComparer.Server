2cd9b21154c0f7bcf4ea90c5096684bf8b91fd54
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2020-12-20 01:44:41 +0100

    Removed unused page uri

--- Helpers/PageUris.cs
+++ Helpers/PageUris.cs
@@ -12,7 +12,6 @@
 		public const string WhatIsUnknown = Show + "?p=WhatIsUnknown";
 		public const string Unknowns = Show + "?p=Unknowns";
 		public const string HowCanIHelp = Show + "?p=HowCanIHelp";
-		public const string GitHub = Show + "?p=GitHub";
 		public const string Changelog = Show + "?p=Changelog";
 		public const string AwesomeSoE = Show + "?p=Awesome";
 		public const string Show = "/show";
