18087b3274b0444e86535ac05bd51725b4eeaeea
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2020-12-23 01:29:02 +0100

    Fixed Menu State bug

--- Shared/NavMenu.razor.cs
+++ Shared/NavMenu.razor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using WebServer.SoE.Services;
 
 namespace WebServer.SoE.Shared
@@ -7,6 +10,7 @@ namespace WebServer.SoE.Shared
 	{
 #nullable disable
 		[Inject] private IAppInfoService AppInfoService { get; set; }
+		[Inject] private ProtectedSessionStorage SessionStorage { get; set; }
 #nullable restore
 
 		private enum ExpandedMenu
@@ -14,25 +18,41 @@ namespace WebServer.SoE.Shared
 			None,
 			SramHacking = 0x1,
 			SramComparison = 0x2,
-			ConsoleApp = 0x4 | SramComparison,
-			WebTools = 0x8 | SramComparison
+			ConsoleApp_Flag = 0x4,
+			ConsoleApp = ConsoleApp_Flag | SramComparison,
+			WebTools_Flag = 0x8,
+			WebTools = WebTools_Flag | SramComparison
 		}
 
-		private static bool _sramHacking = false;
-		private static bool _sramComparison = false;
-		private static bool _consoleApp = false;
-		private static bool _webTools = false;
+		private bool _sramHacking = false;
+		private bool _sramComparison = false;
+		private bool _consoleApp = false;
+		private bool _webTools = false;
+		private ExpandedMenu _menu;
 
-		private void ExpandMenu(ExpandedMenu menu)
+		protected override async Task OnInitializedAsync()
 		{
-			_sramHacking = menu.HasFlag(ExpandedMenu.SramHacking);
+			_menu = (await SessionStorage.GetAsync<ExpandedMenu>(nameof(_menu))).Value;
+			SetMenuStateVariables(_menu);
+		}
+
+		private async Task ExpandMenu(ExpandedMenu menu)
+		{
+			_menu = menu;
+
+			SetMenuStateVariables(menu);
+
+			await SessionStorage.SetAsync(nameof(_menu), _menu);
+		}
+
+		private void SetMenuStateVariables(ExpandedMenu menu)
+		{
 			_sramComparison = menu.HasFlag(ExpandedMenu.SramComparison);
+			_sramHacking = menu.HasFlag(ExpandedMenu.SramHacking);
 			_consoleApp = menu.HasFlag(ExpandedMenu.ConsoleApp);
 			_webTools = menu.HasFlag(ExpandedMenu.WebTools);
-
-			InvokeAsync(StateHasChanged);
 		}
 
-		private void CollapseMenu() => ExpandMenu(ExpandedMenu.None);
+		private Task CollapseMenu() => ExpandMenu(ExpandedMenu.None);
 	}
 }
