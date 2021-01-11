97e856ebbf15c2d11e43ad5f0cc22f018d7e99c2
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-01-11 22:36:11 +0100

    fixed syntax error

--- Helpers/TooltipRandomizer.cs
+++ Helpers/TooltipRandomizer.cs
@@ -4,7 +4,7 @@ namespace WebApp.SoE.Helpers
 {
 	public static class TooltipRandomizer
 	{
-		private static readonly string[] Tooltips = new
+		private static readonly string[] Tooltips = 
 		{
 			"Take heed and go no further",
 			"Camp on the banks of the great green Limpopo River",
@@ -12,7 +12,7 @@ namespace WebApp.SoE.Helpers
 			"I'd turn back if I were you",
 			"Visit beautiful Gruelville, 25 leagues west",
 			"Come see Mr. Head at Perceval Plank's Exhibition of Cultural Oddities",
-			"See the amazing Bearded Boy, next left  (meaning?),
+			"See the amazing Bearded Boy, next left",
 			"Half Way",
 			"City of Costagando, 30 leagues east",
 			 "Mountains of Candy, 50 furlongs" 
