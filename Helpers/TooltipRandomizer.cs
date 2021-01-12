cfdfd2a6a39c071a3516a7782883508eeb89fcae
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-01-12 14:29:44 +0100

    § reworked tooltip randomizer to be thread safe § optics

--- Helpers/TooltipRandomizer.cs
+++ Helpers/TooltipRandomizer.cs
@@ -1,97 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace WebApp.SoE.Helpers
 {
-	public static class TooltipRandomizer
+	public static class TooltipRandomizer 
 	{
 		private const string TooltipFile = "wwwroot/Tooltips.txt";
 		private const int IHaveSpokenWaitTimeInSeconds = 300;
 
-		[ThreadStatic]
 		private static readonly Random Random = new();
-		[ThreadStatic]
 		private static List<string> Tooltips = new();
+		private static object LockObj;
+		private static int ListCount;
+
 		[ThreadStatic]
 		private static DateTimeOffset lastLockedAt;
 		[ThreadStatic]
-		private static int lastLockedIndex;
-		[ThreadStatic]
-		private static object LockObj = new();
+		private static int lastLockedIndex = -1;
 
-		public static string NextTooltip()
+		static TooltipRandomizer()
 		{
-			lock (LockObj)
-			{
-				Initialize();
-				return GetTooltip(Random.Next(Tooltips.Count));
-			}
-		}
-
-		public static string NextMenuTooltip()
-		{
-			lock (LockObj)
-			{
-				Initialize();
-				return GetTooltip(Random.Next(Tooltips.IndexOf(string.Empty)));
-			}
-		}
-
-		public static string GetTooltip(int index)
-		{
+			LockObj ??= new();
 			Initialize();
-			return InternalGetTooltip(index);
 		}
 
-		private static int Initialize()
-		{
-			if (Tooltips.Count == 0)
-			{
-				lock (LockObj)
-				{
-					if (Tooltips.Count > 0) return Tooltips.Count;
+		public static string NextTooltip() => GetTooltip(Random.Next(ListCount));
+		public static string NextMenuTooltip() => GetTooltip(Random.Next(FindIndex(string.Empty)));
+		public static string GetTooltip(int index) => InternalGetTooltip(index);
 
-					try
+		private static void Initialize()
+		{
+			if (ListCount != 0) return;
+
+			lock (LockObj)
+			{
+				if (ListCount > 0) return;
+
+				try
+				{
+					if (File.Exists(TooltipFile))
 					{
-						if (File.Exists(TooltipFile))
-							Tooltips = File.ReadAllLines(TooltipFile).ToList();
-					}
-					catch
-					{
+						var lines = File.ReadAllLines(TooltipFile);
+						if (ListCount > 0) return;
+
+						Tooltips.AddRange(lines);
+						ListCount = lines.Length;
 					}
 				}
+				catch
+				{
+				}
 			}
-
-			return Tooltips.Count;
 		}
 
 		private static string InternalGetTooltip(int index)
 		{
+			if (ListCount == 0) return string.Empty;
+
+			if (index >= ListCount)
+				index = 0;
+
 			try
 			{
-				if ((DateTimeOffset.Now - lastLockedAt).TotalSeconds <= IHaveSpokenWaitTimeInSeconds)
-					index = lastLockedIndex;
-				else
-					lastLockedAt = default;
+				if (lastLockedIndex > -1)
+				{
+					if ((DateTimeOffset.Now - lastLockedAt).TotalSeconds <= IHaveSpokenWaitTimeInSeconds)
+						return Template(GetFromIndex(lastLockedIndex));
+					else
+					{
+						lastLockedAt = default;
+						lastLockedIndex = -1;
+					}
+				}
 
-				if (index >= Tooltips.Count)
-					index = 0;
-
-				var tooltip = Tooltips[index];
-				if (tooltip.Contains("I have spoken."))
+				var tooltip = GetFromIndex(index);
+				if (lastLockedIndex == -1 && tooltip.StartsWith("I have spoken."))
 				{
 					lastLockedIndex = index;
 					lastLockedAt = DateTimeOffset.Now;
 				}
 
-				return $"»{tooltip}«";
+				return Template(tooltip);
 			}
 			catch (Exception ex)
 			{
 				return "Upsi pupsi: " + ex.Message;
 			}
 		}
+
+		private static string GetFromIndex(int index) => Tooltips[index];
+		private static int FindIndex(string text) => Tooltips.IndexOf(text);
+		private static string Template(string text) => $"»{text}«";
 	}
 }
